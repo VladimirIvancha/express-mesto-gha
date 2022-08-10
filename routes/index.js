a83d033b6564508fdaf73827400cd6459b7a1d69
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-08-10 17:48:54 +0300

    fix: not found page fixed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const {
+  ok,
+  notFound,
+} = require('../constants/errorstatuses');
 
 router.get('/', (req, res) => {
-  res.status(200);
+  res.status(ok);
   res.send('Hello World');
 });
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => {
+  res.status(notFound).send({ message: 'Страница не найдена' });
+});
 
 module.exports = router;
