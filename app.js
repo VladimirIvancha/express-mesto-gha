6f6206d3edaf2962af5d36bb36385b352fc63288
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-08-10 17:15:23 +0300

    fix: added bodyParser and several changes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.use((req, res, next) => {
   req.user = {
